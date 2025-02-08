0bfbb9fb78b7060a522ad6dd36e1fd421fb5e9f0
Brian Njogu <briannjogu85@gmail.com>
2025-02-08 21:58:55 +0300

    complete lab

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((res) => res.json())
+      .then((data) => setMovie(data));
+  }, [id]);
+
+  if (!movie) return <h2>Loading...</h2>;
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
+      <NavBar />
+      <h1>{movie.title}</h1>
+      <p>Duration: {movie.time} minutes</p>
+      <h3>Genres:</h3>
+      <div>
+        {movie.genres.map((genre, index) => (
+          <span key={index}>{genre} </span>
+        ))}
+      </div>
     </>
   );
-};
+}
 
 export default Movie;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import ErrorPage from "./pages/ErrorPage";
 
-
-const routes = [
-   
-  ];
+const routes = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/movie/:id", element: <Movie /> },
+  { path: "/actors", element: <Actors /> },
+  { path: "/directors", element: <Directors /> },
+  { path: "*", element: <ErrorPage /> }, // Catch-all route for errors
+]);
 
 export default routes;
